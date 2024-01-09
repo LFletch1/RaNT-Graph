f0b86ce310ec6c39d4e63ce81a920424287eef92
Lance Fletcher <fletcher28@llnl.gov>
2024-01-09 10:02:47 -0800

    Fixing a few bugs, and using different way to access pthis in functors

--- include/RaNT_Graph1.hpp
+++ include/RaNT_Graph1.hpp
@@ -10,22 +10,22 @@
 #include <ygm/detail/ygm_ptr.hpp>
 
 
-// class Default {
-// };
+class Default {
+};
 
 
 // template <typename VertexID, typename RNG>
-template <typename VertexID = uint64_t>
+template <typename UNIQUE = Default, typename VertexID = uint64_t>
 class RaNT_Graph {
 
   /// using self_type          = RaNT_Graph<VertexID, RNG>;
-  using self_type          = RaNT_Graph<VertexID>;
+  using self_type          = RaNT_Graph<UNIQUE, VertexID>;
   using self_ygm_ptr_type  = typename ygm::ygm_ptr<self_type>;
 
   private:
     ygm::comm&   m_comm;
     ygm::container::map<VertexID, std::vector<VertexID>>  m_adj_lists;
-    self_ygm_ptr_type pthis;
+    static self_ygm_ptr_type pthis;
     std::mt19937 m_rng;
 
     ygm::container::counting_set<VertexID>                m_cs;
@@ -49,10 +49,9 @@ class RaNT_Graph {
                   m_local_paths_finished(0),
                   // m_weighted(weighted),
                   // m_directed(directed) 
-                  m_delegation_threshold(d_thresh), // For now make this comm size, can make this user specified
-                  pthis(this)
+                  m_delegation_threshold(d_thresh)  // For now make this comm size, can make this user specified
                   {
-                    // pthis = ygm::ygm_ptr(this);
+                    pthis = ygm::ygm_ptr(this);
 
     }
 
@@ -69,12 +68,9 @@ class RaNT_Graph {
       if (m_delegated_vertex_degrees.find(v) != m_delegated_vertex_degrees.end()) {
         std::uniform_int_distribution<uint64_t> dis(0, m_delegated_vertex_degrees[v]-1);
         uint32_t global_idx = dis(m_rng);
-        m_comm.async(owner(global_idx), 
-                     async_delegated_step(), 
-                     v, 
-                     local_idx(global_idx), path, l, pthis);
+        m_comm.async(owner(global_idx), async_delegated_step(), v, local_idx(global_idx), path, l);
       } else {
-        m_adj_lists.async_visit(v, async_step(), path, l, pthis);
+        m_adj_lists.async_visit(v, async_step(), path, l);
       }
     }
 
@@ -123,18 +119,15 @@ class RaNT_Graph {
     void read_edges_from_container(Container &cont) {
       // Need to check if for_all can be called on container. Do this later
       // For now assume its a bag of std::pair<VertexID, VertexID>
-      ygm::ygm_ptr local_pthis = pthis;
 
-      cont.for_all([&](uint64_t v1, uint64_t v2){
-        if (v1 != v2) {
-          std::vector<uint64_t> adj_list_1 = {v2};
-          std::vector<uint64_t> adj_list_2 = {v1};
-          auto add_to_adjacency_list = [](const auto& key, auto& adj_list, const auto& new_adj_list) {
-            adj_list.push_back(new_adj_list[0]);
-          };
-          pthis->m_adj_lists.async_insert_if_missing_else_visit(v1, adj_list_1, add_to_adjacency_list);
-          pthis->m_adj_lists.async_insert_if_missing_else_visit(v2, adj_list_2, add_to_adjacency_list); 
-        }
+      cont.for_all([](uint64_t v1, uint64_t v2){
+        std::vector<uint64_t> adj_list_1 = {v2};
+        std::vector<uint64_t> adj_list_2 = {v1};
+        auto add_to_adjacency_list = [](const auto& key, auto& adj_list, const auto& new_adj_list) {
+          adj_list.push_back(new_adj_list[0]);
+        };
+        pthis->m_adj_lists.async_insert_if_missing_else_visit(v1, adj_list_1, add_to_adjacency_list);
+        pthis->m_adj_lists.async_insert_if_missing_else_visit(v2, adj_list_2, add_to_adjacency_list); 
       });
 
       m_comm.barrier();
@@ -142,11 +135,10 @@ class RaNT_Graph {
       // Determine which vertices to delegate and remove duplicates from adjacency lists
       uint64_t total_edges = 0;
       std::vector<uint64_t> vertices_to_delegate;
-      // m_adj_lists.for_all([&local_pthis, &total_edges, &vertices_to_delegate](const auto& v, auto& adj_list) {
-      m_adj_lists.for_all([&](const auto& v, auto& adj_list) {
+      m_adj_lists.for_all([&total_edges, &vertices_to_delegate](const auto& v, auto& adj_list) {
         std::sort( adj_list.begin(), adj_list.end() );
         adj_list.erase( std::unique( adj_list.begin(), adj_list.end() ), adj_list.end() );
-        if (adj_list.size() >= local_pthis->m_delegation_threshold) {
+        if (adj_list.size() >= pthis->m_delegation_threshold) {
           vertices_to_delegate.push_back(v);
         }
         total_edges += adj_list.size();
@@ -154,24 +146,24 @@ class RaNT_Graph {
 
       for (auto &v : vertices_to_delegate) {
 
-        auto delegate = [](const auto& v, auto& adj_list, self_ygm_ptr_type local_pthis) {
+        auto delegate = [](const auto& v, auto& adj_list) {
 
-          auto delegator = [](uint64_t v, std::vector<uint64_t> delegated_adj_list, uint64_t degree, self_ygm_ptr_type local_pthis) {
-            local_pthis->m_local_delegated_adj_lists.insert({v, delegated_adj_list});
-            local_pthis->m_delegated_vertex_degrees.insert({v, degree});
+          auto delegator = [](uint64_t v, std::vector<uint64_t> delegated_adj_list, uint64_t degree) {
+            pthis->m_local_delegated_adj_lists.insert({v, delegated_adj_list});
+            pthis->m_delegated_vertex_degrees.insert({v, degree});
           };
           int total = 0;
-          for (int r = 0; r < local_pthis->m_comm.size(); r++) {
+          for (int r = 0; r < pthis->m_comm.size(); r++) {
             std::vector<uint64_t> part_of_adj;
-            for (int i = r; i < adj_list.size(); i += local_pthis->m_comm.size()) {
+            for (int i = r; i < adj_list.size(); i += pthis->m_comm.size()) {
               part_of_adj.push_back(adj_list.at(i));
             }
             total += part_of_adj.size();
-            local_pthis->m_comm.async(r, delegator, v, part_of_adj, adj_list.size(), local_pthis);
+            pthis->m_comm.async(r, delegator, v, part_of_adj, adj_list.size());
           }
           ASSERT_RELEASE(total == adj_list.size());
         };
-        m_adj_lists.async_visit(v, delegate, local_pthis);
+        m_adj_lists.local_visit(v, delegate);
       }
       m_comm.barrier();
 
@@ -183,8 +175,8 @@ class RaNT_Graph {
 
       m_comm.barrier();
 
-      m_adj_lists.for_all([&](const auto& vertex, const auto& adj_list){
-        local_pthis->m_local_vertices.push_back(vertex);
+      m_adj_lists.for_all([](const auto& vertex, const auto& adj_list){
+        pthis->m_local_vertices.push_back(vertex);
       });
 
       for (const auto & [key, value] : m_delegated_vertex_degrees) {
@@ -193,6 +185,13 @@ class RaNT_Graph {
       }
 
       m_comm.barrier();
+
+      // world.barrier();
+      // world.cout0("Total time to delegate vertices: ", timer.elapsed(), " seconds.");
+      // m_comm.cout0();
+      // m_comm.cout0("Total delegated vertices: ", m_local_delegated_adj_lists.size());
+      // world.cout0("Total delegated vertices: ", delegated_graph_adj_lists.size());
+      // world.cout0("Total nondelegated vertices: ", graph_adj_lists.size());
 
       // Need to redisribute edges based on whether they are delegated, can probably do this with
       // taking two passes over the adjacency list data, but will not do this for now.
@@ -203,7 +202,7 @@ class RaNT_Graph {
       
       void operator()(uint64_t vertex,
                         std::vector<uint64_t> adj_list,
-                        std::set<uint64_t> path, uint32_t l, self_ygm_ptr_type pthis) {
+                        std::set<uint64_t> path, uint32_t l) {
         // pthis->m_comm.cout("Made it inside step functor on rank ", pthis->m_comm.rank(), " at vertex ", vertex);
         path.insert(vertex);
         pthis->m_cs.async_insert(vertex);
@@ -226,9 +225,9 @@ class RaNT_Graph {
               pthis->m_comm.async(pthis->owner(global_idx),
                                   async_delegated_step(),
                                   next_vertex,
-                                  pthis->local_idx(global_idx), path, l, pthis);
+                                  pthis->local_idx(global_idx), path, l);
             } else {
-              pthis->m_adj_lists.async_visit(next_vertex, async_step(), path, l, pthis);
+              pthis->m_adj_lists.async_visit(next_vertex, async_step(), path, l);
             }
           } else {
             std::vector<uint64_t> unvisited;
@@ -245,9 +244,9 @@ class RaNT_Graph {
                 pthis->m_comm.async(pthis->owner(global_idx), 
                                     async_delegated_step(), 
                                     next_vertex, 
-                                    pthis->local_idx(global_idx), path, l, pthis);
+                                    pthis->local_idx(global_idx), path, l);
               } else {
-                pthis->m_adj_lists.async_visit(next_vertex, async_step(), path, l, pthis);
+                pthis->m_adj_lists.async_visit(next_vertex, async_step(), path, l);
               }
             } else {
               pthis->m_local_paths_finished++;
@@ -265,7 +264,7 @@ class RaNT_Graph {
       void operator()(uint64_t vertex,
                       uint64_t local_idx,
                       std::set<uint64_t> path,
-                      uint32_t l, self_ygm_ptr_type pthis) {
+                      uint32_t l) {
 
         // pthis->m_comm.cout("Made it inside delegated step functor on rank ", pthis->m_comm.rank(), " at vertex ", vertex);
         path.insert(vertex);
@@ -277,12 +276,9 @@ class RaNT_Graph {
               std::uniform_int_distribution<uint64_t> dis(0, pthis->m_delegated_vertex_degrees[next_vertex]-1);
               uint64_t global_idx = dis(pthis->m_rng);
 
-              pthis->m_comm.async(pthis->owner(global_idx),
-                                  async_delegated_step(), 
-                                  next_vertex, 
-                                  pthis->local_idx(global_idx), path, l, pthis);
+              pthis->m_comm.async(pthis->owner(global_idx), async_delegated_step(), next_vertex, pthis->local_idx(global_idx), path, l);
             } else {
-              pthis->m_adj_lists.async_visit(next_vertex, async_step(), path, l, pthis);
+              pthis->m_adj_lists.async_visit(next_vertex, async_step(), path, l);
             }
           } else {  // Vertex already in path so need to resample another neighbor from original vertex's
                     // the delegated adj list
@@ -291,7 +287,7 @@ class RaNT_Graph {
             pthis->m_comm.async(pthis->owner(global_idx),
                                    async_delegated_step(),
                                    vertex,
-                                   pthis->local_idx(global_idx), path, l, pthis);
+                                   pthis->local_idx(global_idx), path, l);
           }
         } else {
           pthis->m_local_paths_finished++;
--- src/rant_graph_test.cpp
+++ src/rant_graph_test.cpp
@@ -1,10 +1,11 @@
-#include<RaNT_Graph.hpp>
-#include<ygm/container/bag.hpp>
+#include <RaNT_Graph.hpp>
+#include <ygm/container/bag.hpp>
 #include <ygm/comm.hpp>
 #include <ygm/io/line_parser.hpp>
 #include <ygm/random.hpp>
+#include <ygm/utility.hpp>
 
-template<> ygm::ygm_ptr<RaNT_Graph<>> RaNT_Graph<>::pthis = NULL;
+// template<> ygm::ygm_ptr<RaNT_Graph<>> RaNT_Graph<>::pthis = NULL;
 // template<> ygm::ygm_ptr<RaNT_Graph<long>> RaNT_Graph<long>::pthis = NULL;
 
 int main(int argc, char** argv) {
@@ -18,78 +19,73 @@ int main(int argc, char** argv) {
     // RaNT_Graph<uint64_t, ygm_rng> rant_graph = RaNT_Graph<uint64_t, ygm_rng>(world, rng);
     // MyObject *MyClass::myObject;
     // RaNT_Graph 
-    RaNT_Graph rant_graph = RaNT_Graph(world);
-    // RaNT_Graph<int> rant_graph = RaNT_Graph<int>(world);
-    // RaNT_Graph<long> rant_graph2 = RaNT_Graph<long>(world);
 
     ygm::container::bag<std::pair<uint64_t,uint64_t>> graph_edges(world);
+    ygm::timer timer;
     // std::string filename = "/g/g20/lancef/graphs/facebook_combined.txt";
-    std::string filename = "/g/g20/lancef/graphs/karate_club.txt";
+    // std::string filename = "/g/g20/lancef/graphs/karate_club.txt";
+    std::string filename = "/p/lustre2/havoqgtu/LiveJournal/edge.txt";
+    // std::string filename = "/p/lustre2/havoqgtu/trevor_twitter";
+    // std::string filename = "/p/lustre2/havoqgtu/trevor_twitter/twitter_rv.net_1_of_288";
+    // std::string filename = "/p/lustre2/havoqgtu/trevor_twitter";
+    // std::string filename = "/p/lustre2/havoqgtu/trevor_uk-2007-05/uk-2007-05.edgelist_1_of_288";
+
+    world.cout0("Reading graph edges into bag");
     ygm::io::line_parser file_reader(world, {filename});
-    file_reader.for_all([&graph_edges](const std::string& line) {
+    file_reader.for_all([&graph_edges, &world](const std::string& line) {
       std::string token1, token2; 
-      size_t pos = line.find(" ");
-      if (pos != std::string::npos) {
-          std::string token1 = line.substr(0, pos);
-        //   std::string token2 = line.substr(pos + delim.length());
-          std::string token2 = line.substr(pos + 1);
-          std::stringstream converter1(token1);
-          std::stringstream converter2(token2);
-          uint64_t v1, v2;
-          converter1 >> v1;
-          converter2 >> v2;
-          graph_edges.async_insert(std::make_pair(v1, v2));
-      }
+      uint64_t v1, v2;
+      std::stringstream str_stream(line);
+      str_stream >> v1;
+      str_stream >> v2;
+      graph_edges.async_insert(std::make_pair(v1, v2));
     }); 
 
+    world.barrier();
+
+    // graph_edges.for_all([&world](auto &pair){
+    //   // world.cout(pair.first, ", ", pair.second);
+    //   // if (pair.first == pair.second && world.rank0()) {
+    //   if (world.rank0()) {
+    //     world.cout(pair.first, ", ", pair.second);
+    //   }
+    // });
+    // return 0;
+
+    // RaNT_Graph rant_graph = RaNT_Graph(world, world.size());
+    RaNT_Graph rant_graph = RaNT_Graph(world, 24);
+    // RaNT_Graph<int> rant_graph2 = RaNT_Graph<int>(world);
+    // RaNT_Graph<long> rant_graph2 = RaNT_Graph<long>(world);
+    world.cout0("Time needed to read graph edges into bag: ", timer.elapsed());
+    world.cout0("Bag size: ", graph_edges.size());
+    // return 0;
+    world.barrier();
+
+    timer.reset();
     rant_graph.read_edges_from_container(graph_edges);
     // rant_graph2.read_edges_from_container(graph_edges);
     world.barrier();
+    world.cout0("Total time to ingest graph from bag: ", timer.elapsed(), " seconds.");
+    world.cout0("Number of vertices: ", rant_graph.num_vertices());
  
     world.cout0("Taking walks on 1st RaNT-Graph");
     world.barrier();
 
-    rant_graph.take_n_paths(4, 10);
-    // if (world.rank0()) {
-    //   rant_graph.start_walk(10); 
+    timer.reset();
+    // uint32_t n_paths = 100000000;
+    // int p = 1000000;
+    // for (int i = 1; i <= 100; i++) {
+    //   rant_graph.take_n_paths(p, 21);
+    //   world.cout0("Total time to take ", p*i, " paths: ", timer.elapsed(), " seconds.");
     // }
+    // uint32_t n_paths = 580000000;
+    uint32_t n_paths = 10000000;
+    // rant_graph.take_n_paths(n_paths, 21);
+    rant_graph.take_n_paths(n_paths, 21);
 
-    // world.barrier();
-    // world.cout0("Now taking walks on 2nd RaNT-Graph");
-    // if (world.rank0()) {
-    //   rant_graph2.start_walk(10); 
-    // }
-    // world.cout() << "I am rank: " << world.rank() << std::endl;   
-    
-    /*
-    ygm::container::bag<std::pair<uint64_t,uint64_t>> graph_edges(world);
-    // std::string filename = "/g/g20/lancef/graphs/facebook_combined.txt";
-    std::string filename = "/g/g20/lancef/graphs/test_tabs.txt";
-    ygm::io::line_parser file_reader(world, {filename});
-    file_reader.for_all([&graph_edges](const std::string& line) {
-      std::string token1, token2; 
-      size_t pos = line.find(" ");
-      if (pos != std::string::npos) {
-          std::string token1 = line.substr(0, pos);
-        //   std::string token2 = line.substr(pos + delim.length());
-          std::string token2 = line.substr(pos + 1);
-          std::stringstream converter1(token1);
-          std::stringstream converter2(token2);
-          uint64_t v1, v2;
-          converter1 >> v1;
-          converter2 >> v2;
-          graph_edges.async_insert(std::make_pair(v1, v2));
-      }
-    }); 
-
-    rant_graph.read_edges_from_container(graph_edges);
-
-    world.cout0() << "Number of vertices in RaNT-Graph: " << rant_graph.num_vertices() << std::endl;
-
-    rant_graph.take_n_paths(1);
-
-    world.cout0() << "Number of paths finished: " << rant_graph.paths_finished() << std::endl;
-    */
+    world.barrier();
+    world.cout0("Total time to take ", n_paths, " paths: ", timer.elapsed(), " seconds.");
+    // ASSERT_RELEASE(rant_graph.paths_finished() == n_paths);
 
     return 0;
 }
